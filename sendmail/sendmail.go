eef62aa407e38088fc6b631624d42ac3754a538b
asmolin <andreismolin@yandex.ru>
2022-11-22 01:25:36 +0300

    changet update status

--- sendmail/sendmail.go
+++ sendmail/sendmail.go
@@ -6,35 +6,6 @@ import (
 
 func Sendassigned(ticket string) {
 
-	// from := "asmolin@soyuzintegro.ru"
-
-	// user := "asmolin"
-	// password := "Oadnpvia04!!"
-
-	// to := []string{
-	// 	"asmolin@soyuzintegro.ru",
-	// 	"operu_it_inbox_robot@gkm.ru",
-	// 	"mariya.litvinova@gkm.ru",
-	// }
-
-	// addr := "mail.soyuzintegro.ru:25"
-	// host := "mail.soyuzintegro.ru"
-
-	// msg := []byte("From: megapolis@soyuzintegro.ru\r\n" +
-	// 	"To: operu_it_inbox_robot@gkm.ru\r\n" +
-	// 	"Subject: Incident/" + ticket + "/Assigned\r\n\r\n" +
-	// 	"<incident><number>" + ticket + "</number><nameCharge>Богданова Светлана</nameCharge><phoneCharge>+7 (900) 500-00-00</phoneCharge></incident>\r\n")
-
-	// auth := smtp.PlainAuth("", user, password, host)
-
-	// err := smtp.SendMail(addr, auth, from, to, msg)
-
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// fmt.Println("Email sent successfully")
-
 	m := gomail.NewMessage()
 	m.SetHeader("From", "megapolis@soyuzintegro.ru")
 	m.SetHeader("To", "asmolin@soyuzintegro.ru")
@@ -54,7 +25,7 @@ func SendStartWork(ticket string) {
 	m := gomail.NewMessage()
 	m.SetHeader("From", "megapolis@soyuzintegro.ru")
 	m.SetHeader("To", "asmolin@soyuzintegro.ru")
-	m.SetHeader("Subject", "Incident/"+ticket+"/Assigned\r\n\r\n")
+	m.SetHeader("Subject", "Incident/"+ticket+"/Update\r\n\r\n")
 	m.SetBody("text/plain", `<?xml version="1.0" encoding="UTF-8"?><incident><number>`+ticket+`</number><incidentId>691458</incidentId><status>В работе</status></incident>`)
 
 	// Send the email to Bob
@@ -70,7 +41,7 @@ func SendEndWork(ticket string) {
 	m := gomail.NewMessage()
 	m.SetHeader("From", "megapolis@soyuzintegro.ru")
 	m.SetHeader("To", "asmolin@soyuzintegro.ru")
-	m.SetHeader("Subject", "Incident/"+ticket+"/Assigned\r\n\r\n")
+	m.SetHeader("Subject", "Incident/"+ticket+"/Update\r\n\r\n")
 	m.SetBody("text/palin", `<?xml version="1.0" encoding="UTF-8"?><incident><number>`+ticket+`</number><comment>[title:Администратор СоюзИнтегро]: Задание 682406/480091 мастер отчитался о завершении работ</comment></incident>`)
 
 	// Send the email to Bob
